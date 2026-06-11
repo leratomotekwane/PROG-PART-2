2526bd1a55b45a6721577ffaa57de06bc82f2e97
leratomotekwane <166137659+leratomotekwane@users.noreply.github.com>
2026-06-11 16:12:08 +0200

    Update Login.java

--- src/main/java/com/mycompany/quickchart/Login.java
+++ src/main/java/com/mycompany/quickchart/Login.java
@@ -1,17 +1,19 @@
 package com.mycompany.quickchart;
 
+
 class Login {
-    static User registeredUser = new User();
+    static String regUsername = "";
+    static String regPassword = "";
+    static String regCellphone = "";
+    static String regFirstname = "";
+    static String regLastname = "";
     static boolean isLoggedIn = false;
-    
-    static boolean checkUserName(String username){
-        if(username.length() > 5 || !username.contains("_")){
-            return false;
-        }
-        return true;
+ 
+    static boolean checkUserName(String username) {
+        return username.contains("_") && username.length() <= 5;
     }
     
-    static boolean checkPasswordComplexity(String password){
+    static boolean checkPasswordComplexity(String password) {
         if(password.length() < 8) return false;
         if(password.equals(password.toLowerCase())) return false;
         if(!password.matches(".*[!@#$%^&*()_+].*")) return false;
@@ -19,63 +21,53 @@ class Login {
         return true;
     }
     
-    static boolean checkCellPhoneNumber(String cellphone){
-        if(cellphone.startsWith("+27") && cellphone.length() == 12){
-            String numbers = cellphone.substring(3);
-            if(numbers.matches("\\d+")){
+    static boolean checkCellPhoneNumber(String cellphone) {
+        if(cellphone.startsWith("+27") && cellphone.length() == 12) {
+            String number = cellphone.substring(3);
+            if(number.matches("\\d+")) {
                 return true;
             }
         }
         return false;
     }
     
-    static String registerUser(String firstName, String lastName, String username, String password, String cellphone){
-        String errors = "";
+    static String registerUser(String firstname, String lastname, String username, String password, String cellphone) {
+        String error = "";
         
-        if(!checkUserName(username)){
-            errors += "Username is not correctly formatted; please ensure that your username contains an underscore and is no more than five characters length.\n";
+        if(!checkUserName(username)) {
+            error += "Username is not correctly formatted; please ensure that your username contains an underscore and is no more than five characters length.\n";
+        }
+        if(!checkPasswordComplexity(password)) {
+            error += "Password is not correctly formatted; please ensure that the password contains at least eight characters, a capital letter, a number and a special character.\n";
+        }
+        if(!checkCellPhoneNumber(cellphone)) {
+            error += "Cellphone is not correctly formatted or does not contain international code.\n";
         }
         
-        if(!checkPasswordComplexity(password)){
-            errors += "Password is not correctly formatted; please ensure that the password contains at least eight characters, a capital letter, a number and a special character.\n";
+        if(!error.isEmpty()) {
+            return error;
         }
         
-        if(!checkCellPhoneNumber(cellphone)){
-            errors += "Cellphone is not correctly formatted or does not contain international code.\n";
-        }
+        regUsername = username;
+        regPassword = password;
+        regFirstname = firstname;
+        regLastname = lastname;
+        regCellphone = cellphone;
         
-        if(!errors.isEmpty()){
-            return errors;
-        }
-        
-        String success = "";
-        success += "Username successfully captured\n";
-        success += "Password successfully captured\n";
-        success += "Cellphone successfully captured\n";
-        success += "Welcome " + firstName + " " + lastName + " it is great to see you.";
-        
-        registeredUser.username = username;
-        registeredUser.password = password;
-        registeredUser.firstName = firstName;
-        registeredUser.lastName = lastName;
-        registeredUser.cellphone = cellphone;
-        
-        return success;
+        return "Username successfully captured\nPassword successfully captured\nCellphone successfully captured\nWelcome " + firstname + " " + lastname + " it is great to see you.";
     }
-    
-    static boolean loginUser(String username, String password){
-        boolean success = username.equals(registeredUser.username) && password.equals(registeredUser.password);
+ 
+    static boolean loginUser(String username, String password) {
+        boolean success = username.equals(regUsername) && password.equals(regPassword);
         isLoggedIn = success;
         return success;
     }
-    
-    static String returnLoginStatus(boolean isLoggedIn) {
-        if(isLoggedIn) {
-            String firstName = registeredUser.firstName != null ? registeredUser.firstName : "";
-            String lastName = registeredUser.lastName != null ? registeredUser.lastName : "";
-            return "Welcome Back " + firstName + " " + lastName;
+   
+    static String returnLoginStatus(boolean logged) {
+        if(logged) {
+            return "Welcome back " + regFirstname + " " + regLastname + " it is great to see you again";
         } else {
-            return "Login has failed";
+            return "A failed login";
         }
     }
 }
